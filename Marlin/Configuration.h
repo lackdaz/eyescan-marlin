3772904493a09b15ce869f8104a610ff1e4fb440
Seth Loh <lackdaz@gmail.com>
2021-10-11 19:15:26 +0800

    eyescan implemented [untested]

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -889,9 +889,9 @@
  *          TMC5130, TMC5130_STANDALONE, TMC5160, TMC5160_STANDALONE
  * :['A4988', 'A5984', 'DRV8825', 'LV8729', 'L6470', 'L6474', 'POWERSTEP01', 'TB6560', 'TB6600', 'TMC2100', 'TMC2130', 'TMC2130_STANDALONE', 'TMC2160', 'TMC2160_STANDALONE', 'TMC2208', 'TMC2208_STANDALONE', 'TMC2209', 'TMC2209_STANDALONE', 'TMC26X', 'TMC26X_STANDALONE', 'TMC2660', 'TMC2660_STANDALONE', 'TMC5130', 'TMC5130_STANDALONE', 'TMC5160', 'TMC5160_STANDALONE']
  */
-#define X_DRIVER_TYPE A4988
-#define Y_DRIVER_TYPE A4988
-#define Z_DRIVER_TYPE A4988
+#define X_DRIVER_TYPE TMC2209
+#define Y_DRIVER_TYPE TMC2209
+#define Z_DRIVER_TYPE TMC2209
 //#define X2_DRIVER_TYPE A4988
 //#define Y2_DRIVER_TYPE A4988
 //#define Z2_DRIVER_TYPE A4988
@@ -900,7 +900,7 @@
 //#define I_DRIVER_TYPE  A4988
 //#define J_DRIVER_TYPE  A4988
 //#define K_DRIVER_TYPE  A4988
-#define E0_DRIVER_TYPE A4988
+#define E0_DRIVER_TYPE TMC2209
 //#define E1_DRIVER_TYPE A4988
 //#define E2_DRIVER_TYPE A4988
 //#define E3_DRIVER_TYPE A4988
@@ -955,9 +955,9 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT \
-    {                               \
-        80, 80, 400, 500            \
+#define DEFAULT_AXIS_STEPS_PER_UNIT    \
+    {                                  \
+        200, 200, 200, 314.9606299 / 2 \
     }
 
 /**
@@ -967,8 +967,8 @@
  */
 #define DEFAULT_MAX_FEEDRATE \
     {                        \
-        300, 300, 5, 25      \
-    }
+        300, 300, 300, 5     \
+    } // TODO: please test for ext.
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -986,8 +986,8 @@
  */
 #define DEFAULT_MAX_ACCELERATION \
     {                            \
-        3000, 3000, 100, 10000   \
-    }
+        3000, 3000, 100, 5       \
+    } //TODO: please test for extrusion
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -1005,9 +1005,10 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION 3000         // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION 3000 // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION 3000  // X, Y, Z acceleration for travel (non printing) moves
+// TODO: are these sensible default values?
+#define DEFAULT_ACCELERATION 500         // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION 500 // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION 500  // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)
@@ -1371,7 +1372,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
-#define INVERT_Y_DIR true
+#define INVERT_Y_DIR false
 #define INVERT_Z_DIR false
 //#define INVERT_I_DIR false
 //#define INVERT_J_DIR false
@@ -1418,16 +1419,17 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 96
+#define Y_BED_SIZE 38
 
+// TODO: set physical limits here
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 45
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
@@ -1800,10 +1802,10 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_MM_M           \
-    {                                  \
-        (50 * 60), (50 * 60), (4 * 60) \
-    }
+#define HOMING_FEEDRATE_MM_M            \
+    {                                   \
+        (30 * 60), (30 * 60), (30 * 60) \
+    } // TODO: set sensible defaults here
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2625,7 +2625,7 @@
  * https://github.com/teemuatlut/TMCStepper
  */
 #if HAS_TRINAMIC_CONFIG
-
+#define CUSTOM_MICROSTEP 4
 #define HOLD_MULTIPLIER 0.5 // Scales down the holding current from run current
 
 /**
@@ -2635,9 +2635,9 @@
 #define INTERPOLATE true
 
 #if AXIS_IS_TMC(X)
-#define X_CURRENT 800            // (mA) RMS current. Multiply by 1.414 for peak current.
-#define X_CURRENT_HOME X_CURRENT // (mA) RMS current for sensorless homing
-#define X_MICROSTEPS 16          // 0..256
+#define X_CURRENT 564                 // (mA) RMS current. Multiply by 1.414 for peak current.
+#define X_CURRENT_HOME X_CURRENT      // (mA) RMS current for sensorless homing
+#define X_MICROSTEPS CUSTOM_MICROSTEP // 0..256
 #define X_RSENSE 0.11
 #define X_CHAIN_POS -1 // -1..0: Not chained. 1: MCU MOSI connected. 2: Next in chain, ... \
                        //#define X_INTERPOLATE  true      // Enable to override 'INTERPOLATE' for the X axis
@@ -2653,16 +2653,16 @@
 #endif
 
 #if AXIS_IS_TMC(Y)
-#define Y_CURRENT 800
+#define Y_CURRENT 564
 #define Y_CURRENT_HOME Y_CURRENT
-#define Y_MICROSTEPS 16
+#define Y_MICROSTEPS CUSTOM_MICROSTEP
 #define Y_RSENSE 0.11
 #define Y_CHAIN_POS -1
 //#define Y_INTERPOLATE  true
 #endif
 
 #if AXIS_IS_TMC(Y2)
-#define Y2_CURRENT 800
+#define Y2_CURRENT 354
 #define Y2_CURRENT_HOME Y2_CURRENT
 #define Y2_MICROSTEPS Y_MICROSTEPS
 #define Y2_RSENSE 0.11
@@ -2671,9 +2671,9 @@
 #endif
 
 #if AXIS_IS_TMC(Z)
-#define Z_CURRENT 800
+#define Z_CURRENT 564
 #define Z_CURRENT_HOME Z_CURRENT
-#define Z_MICROSTEPS 16
+#define Z_MICROSTEPS CUSTOM_MICROSTEP
 #define Z_RSENSE 0.11
 #define Z_CHAIN_POS -1
 //#define Z_INTERPOLATE  true
@@ -2734,8 +2734,8 @@
 #endif
 
 #if AXIS_IS_TMC(E0)
-#define E0_CURRENT 800
-#define E0_MICROSTEPS 16
+#define E0_CURRENT 80
+#define E0_MICROSTEPS CUSTOM_MICROSTEP
 #define E0_RSENSE 0.11
 #define E0_CHAIN_POS -1
 //#define E0_INTERPOLATE true
@@ -3030,7 +3030,7 @@
    * Enable M122 debugging command for TMC stepper drivers.
    * M122 S0/1 will enable continuous reporting.
    */
-//#define TMC_DEBUG
+#define TMC_DEBUG
 
 /**
    * You can set your own advanced settings by filling in predefined functions.
